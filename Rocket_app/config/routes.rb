4215dc1640a02a2993020ceeeb42cef99bfba25e
artififcialsoda <dimitrov.fabien@gmail.com>
2020-10-14 17:23:07 -0400

    Initialize backoffice

--- Rocket_app/config/routes.rb
+++ Rocket_app/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 
 end
