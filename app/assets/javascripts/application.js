a68387cea88538ca84996cfcb946632f68079a7d
trevorius <trevorius45@hotmail.com>
2020-10-16 13:43:50 -0400

    revert bqck to no errors on head change

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,16 +11,19 @@
 // about supported directives.
 //
 
-
 //= require jquery
 //= require jquery_ujs
 //= require popper
 //= require bootstrap
+//= require pages/demo.revolution_slider
+//= require pages/jquery.themepunch.revolution.min
+//= require pages/jquery.themepunch.tools.min
+//= require pages/jquery.mixitup.min
+//= require pages/owl.carousel
+//= require quote
+//= require pages/jquery.themepunch.enablelog
+//= require pages/smoothscroll
+//= require pages/jquery.nav.min
+//= require pages/scripts
+
 //= require turbolinks
-
-
-//= require gmaps
-
-
-//= require_tree .
-
