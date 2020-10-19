88ed6a4831e5aa107f5b53ab1c08ad552d4b9129
vnga-dao <dao.vnga@gmail.com>
2020-10-19 10:28:04 -0400

    employees_created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :addresses
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -8,6 +9,7 @@ Rails.application.routes.draw do
   resource :quotes 
 
   get 'quote' => 'quotes#quote'
+  get 'address' => 'addresses#address'
   get 'corporate' => 'pages#corporate'
   get 'residential' => 'pages#residential'
   get 'index' => 'pages#index'
--- db/migrate/20201018205605_change_int_to_double.rb
+++
@@ -1,7 +0,0 @@
-class ChangeIntToDouble < ActiveRecord::Migration[5.2]
-  def change
-    change_column :quotes, :elevator_cost, :float(2)
-    change_column :quotes, :installation_cost, :float(2)
-    change_column :quotes, :total_cost, :float(2)
-  end
-end
