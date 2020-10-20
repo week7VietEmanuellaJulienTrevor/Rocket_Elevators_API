bb20b9f8515f94604880698a2444d681a030fca7
trevorius <trevorius45@hotmail.com>
2020-10-19 21:48:44 -0400

    add country-selectorgem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_19_230343) do
+ActiveRecord::Schema.define(version: 2020_10_20_012708) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "namespace"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2020_10_19_230343) do
     t.string "suite_or_apartment"
     t.string "city"
     t.string "postal_code"
-    t.string "nation"
+    t.string "country"
     t.string "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
