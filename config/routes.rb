a702aa06770ecff3b56ac139d46de615d0c0c9ab
artififcialsoda <dimitrov.fabien@gmail.com>
2020-10-21 16:34:31 -0400

    Update backoffice login page - signup functionality is now added

--- config/routes.rb
+++ config/routes.rb
@@ -19,13 +19,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 
+  # GET routes
   get 'quote' => 'quotes#quote'
-  # get 'address' => 'addresses#address'
-  # get 'customer' => 'customer#customer'
   get 'corporate' => 'pages#corporate'
   get 'residential' => 'pages#residential'
   get 'index' => 'pages#index'
 
+  # POST routes
   post 'quote/create' => 'quotes#create'
   post 'contact/create' => 'leads#create'
 end
