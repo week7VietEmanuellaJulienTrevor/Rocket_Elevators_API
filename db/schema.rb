d00d2bfe091710d0ed2bfd2d7d16dc5217299cf3
trevorius <trevorius45@hotmail.com>
2020-10-22 18:26:38 -0400

    change notes type to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_22_220638) do
+ActiveRecord::Schema.define(version: 2020_10_22_222125) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "namespace"
@@ -68,7 +68,7 @@ ActiveRecord::Schema.define(version: 2020_10_22_220638) do
 
   create_table "building_details", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "information_key"
-    t.string "value"
+    t.text "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "building_id"
@@ -144,7 +144,7 @@ ActiveRecord::Schema.define(version: 2020_10_22_220638) do
     t.date "last_inspection_date"
     t.string "inspection_certificate"
     t.string "information"
-    t.string "notes"
+    t.text "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "column_id"
