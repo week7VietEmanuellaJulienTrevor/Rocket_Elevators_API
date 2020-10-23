486a97a8116459c41b96c7b06c3dd3ae86f641ad
trevorius <trevorius45@hotmail.com>
2020-10-23 11:51:21 -0400

    tried changing db name and db_second works on my local

--- config/initializers/db_second.rb
+++ config/initializers/db_second.rb
@@ -1,4 +1,5 @@
 # save stats database settings in global var
 # DB_STATS = YAML::load(ERB.new(File.read(Rails.root.join("config","database_second.yml"))).result)[Rails.env]
-config=YAML::load(ERB.new(File.read(Rails.root.join(“config”,“database_second.yml”))).result)
+config =YAML::load(ERB.new(File.read(Rails.root.join("config","database_second.yml"))).result)
+
 DB_SECOND = config[Rails.env]
