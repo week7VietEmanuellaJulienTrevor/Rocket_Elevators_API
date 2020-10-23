af136693b82e947d27233e2b334eb43239ab091b
artififcialsoda <dimitrov.fabien@gmail.com>
2020-10-23 19:26:02 -0400

    Remove unnecessary admin_role column in AdminUsers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_220046) do
+ActiveRecord::Schema.define(version: 2020_10_23_232206) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "namespace"
@@ -45,7 +45,6 @@ ActiveRecord::Schema.define(version: 2020_10_23_220046) do
     t.string "encrypted_password", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin_role", default: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
