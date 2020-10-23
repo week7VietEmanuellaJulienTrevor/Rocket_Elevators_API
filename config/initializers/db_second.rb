370eb7c5dd074795311c30d9e5a695d0820245da
trevorius <trevorius45@hotmail.com>
2020-10-23 11:30:50 -0400

    change db_second

--- config/initializers/db_second.rb
+++ config/initializers/db_second.rb
@@ -1,2 +1,4 @@
 # save stats database settings in global var
-DB_STATS = YAML::load(ERB.new(File.read(Rails.root.join("config","database_second.yml"))).result)[Rails.env]
+# DB_STATS = YAML::load(ERB.new(File.read(Rails.root.join("config","database_second.yml"))).result)[Rails.env]
+config=YAML::load(ERB.new(File.read(Rails.root.join(“config”,“database_second.yml”))).result)
+DB_SECOND = config[Rails.env]
