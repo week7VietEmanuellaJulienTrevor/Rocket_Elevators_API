41aaddc2cc6f64ada33ac2b20168bb2712a72cbc
trevorius <trevorius45@hotmail.com>
2020-10-23 20:53:18 -0400

    pb with migrations

--- db/migrate/20201023151934_add_previously_removed_misc_columns_to_admin_user.rb
+++ db/migrate/20201023151934_add_previously_removed_misc_columns_to_admin_user.rb
@@ -1,13 +1,13 @@
 class AddPreviouslyRemovedMiscColumnsToAdminUser < ActiveRecord::Migration[5.2]
   def up
-    add_column :admin_users, :reset_password_token, :string 
-    add_column :admin_users, :reset_password_sent_at, :datetime
-    add_column :admin_users, :remember_created_at, :datetime
+    #add_column :admin_users, :reset_password_token, :string 
+    #add_column :admin_users, :reset_password_sent_at, :datetime
+    #add_column :admin_users, :remember_created_at, :datetime
   end
 
   def down
-    remove_column :admin_users, :reset_password_token
-    remove_column :admin_users, :reset_password_sent_at
-    remove_column :admin_users, :remember_created_at
+    #remove_column :admin_users, :reset_password_token
+    #remove_column :admin_users, :reset_password_sent_at
+    #remove_column :admin_users, :remember_created_at
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -45,9 +45,6 @@ ActiveRecord::Schema.define(version: 2020_10_23_232206) do
     t.string "encrypted_password", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.index ["email"], name: "index_admin_users_on_email", unique: true
   end
 
