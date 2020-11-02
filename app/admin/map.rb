60f5a3181a7fe688683352117251181bb29a8bf3
Jdup29 <judup13@gmail.com>
2020-11-02 10:50:28 -0500

    maps julien tries

--- app/admin/map.rb
+++ app/admin/map.rb
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 ActiveAdmin.register_page "Map" do
     content do
       render partial: 'map'
     end
   end
-=======
-ActiveAdmin.register_page "map" do
-    content do
-        render partial: 'map'
-    end
-    
-end
-  
->>>>>>> origin/trevor
