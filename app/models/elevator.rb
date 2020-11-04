97f79806d47922be8c8d0cc1084a30fe6ce7ec70
vnga-dao <dao.vnga@gmail.com>
2020-11-03 22:02:33 -0500

    twilio_done

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -1,16 +1,30 @@
-require './lib/tasks/messenger.rb'
+# require './lib/tasks/messenger.rb'
+require 'twilio-ruby'
 
 class Elevator < ApplicationRecord
     belongs_to :column
     belongs_to :customer
-    after_update :send_sms
+    after_save :send_sms
 
     def send_sms()
-        status = self.status
-        if status == 'intervention'
-            twiliosms = Messenger::Twilio.new 
-            twiliosms.send_sms
+        acct_sid = ENV['TWILIO_ACCT_SID']
+        auth_token = ENV['TWILIO_AUTH']
+
+        @client = ::Twilio::REST::Client.new acct_sid, auth_token
+
+        from = ENV['TWILIO_PHONE']
+        to = "+14388218911"
+        body = ''
+
+        if self.status.downcase == 'intervention'
+            body = "The Elevator " + (self.id).to_s + " with Serial Number " + (self.serial_number).to_s + " changed status to intervention"
         end
+
+        message = @client.messages.create(
+            :from => from,
+            :to => to,
+            :body => body
+        )
     end
     
 end
--- lib/tasks/messenger.rb
+++ lib/tasks/messenger.rb
@@ -1,19 +1,25 @@
-module Messenger
-    class Twilio
-        def send_sms(number)
-            acct_sid = ENV['TWILIO_ACCT_SID']
-            auth_token = ENV['TWILIO_AUTH']
+# require 'twilio-ruby'
 
-            @client = Twilio::REST::Client.new acct_sid, auth_token
+# module Messenger
+#     class Twilio
+#         def send_sms
+#             acct_sid = ENV['TWILIO_ACCT_SID']
+#             auth_token = ENV['TWILIO_AUTH']
 
-            from = ENV['TWILIO_PHONE']
+#             @client = ::Twilio::REST::Client.new acct_sid, auth_token
 
-            message = @client.account.messages.create(
-                :from => from,
-                :to => '+14388218911',
-                :body => 'Elevator status has been changed to Intervention ' 
-            )
-        end 
-    end
-end
+#             from = ENV['TWILIO_PHONE']
+#             to = "+14388218911"
+
+#             message = @client.messages.create(
+#                 :from => from,
+#                 :to => to,
+#                 # :body => 'Elevator status has been changed to Intervention' 
+#                 if @Elevator[:status] == 'intervention'
+#                 :body => ("The Elevator " + @Elevator[:id] + " with Serial Number " + @Elevator[:serial_number] + " changed status from "+ @Elevator.find[:status_was] + " to " + @Elevator.find[:status]).to_s
+#                 end
+#             )
+#         end 
+#     end
+# end
     
