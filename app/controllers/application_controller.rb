81b2607ca771d23c8666ddf03b568299cbf7c8b4
vnga-dao <dao.vnga@gmail.com>
2020-11-06 12:08:11 -0500

    still try 7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    skip_before_filter :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'create' }
+    skip_before_action :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'create' }
     
     def access_denied(exception)
         redirect_to index_path, alert: exception.message
