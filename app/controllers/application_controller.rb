603b2390a9e00cc57e5caefd5a0b9ff7f1adfda7
vnga-dao <dao.vnga@gmail.com>
2020-11-06 12:05:37 -0500

    still try secure6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery prepend: true, with: :exception
+    skip_before_filter :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'create' }
     
     def access_denied(exception)
         redirect_to index_path, alert: exception.message
