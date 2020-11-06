ff33c6ac5f027d6d4d62727650e9fd9d4e85fed5
vnga-dao <dao.vnga@gmail.com>
2020-11-06 11:44:25 -0500

    to make website secure

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,6 @@ Rails.application.configure do
 
   # Host
   # routes.default_url_options[:host] = 'rocket-elevators-trevor.tk'
-  config.force_ssl = true
   config.action_mailer.default_url_options = { host: "https://vndaos.com/" }
 end
 
