7ed0db26e9ed7d7a72e8e7823ca820ac4b3eefcd
vnga-dao <dao.vnga@gmail.com>
2020-11-06 12:23:17 -0500

    try with elevator

--- app/controllers/elevators_controller.rb
+++ app/controllers/elevators_controller.rb
@@ -1,5 +1,7 @@
 
 class ElevatorsController < InheritedResources::Base
+  skip_before_action :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'create' }
+
 
   def index
     @elevators = Elevator.all
