94be717f237354f17c8417d4d1a05bebcd0538ed
vnga-dao <dao.vnga@gmail.com>
2020-11-06 13:26:16 -0500

    try https

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    skip_before_action :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'create' }
+    # skip_before_action :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'create' }
     # skip_before_action :verify_authenticity_token, if: -> { controller_name == 'elevators' && action_name == 'index' }
 
     def access_denied(exception)
