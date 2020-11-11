1d6684aaf76853653f52412259dc0cbc6c0d1322
Emmanuellad <emmanuella0524@yahoo.com>
2020-11-11 08:59:47 -0500

    Revert "graphql database not setup"
    This reverts commit fb738e32017ad3f14658a9d59c07aec8ee17d14c.

--- app/graphql/types/building_type.rb
+++ app/graphql/types/building_type.rb
@@ -1,15 +1,5 @@
 module Types
   class BuildingType < Types::BaseObject
-     field :building_details, [BuildingDetailType], null: false
-      field :customer, CustomerType, null: false
-      field :address, AddressType, null: false
-      field :building_administrator_full_name, String, null: false
-      field :building_administrator_email, String, null: false
-      field :building_administrator_phone, String, null: false
-      field :building_technical_contact_name, String, null: false
-      field :building_technical_contact_email, String, null: false
-      field :building_technical_contact_phone, String, null: false
-      field :interventions, [InterventionType], null: true
-      
+    
   end
 end
