7fd92b7bc4422494adf7387c3188ab5af938ca64
trevorius <trevorius45@hotmail.com>
2020-11-15 14:58:33 -0500

    created partial

---
+++ app/admin/customerinformation.rb
@@ -0,0 +1,5 @@
+ActiveAdmin.register_page "Customer Information" do
+    content do
+      render partial: 'customerinformation'
+    end
+  end
--- app/admin/factquotes.rb
+++ app/admin/factquotes.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Factquote do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :creation, :company_name, :email, :nb_elevator
+  permit_params :creation, :company_name, :email, :nb_elevator
   #
   # or
   #
